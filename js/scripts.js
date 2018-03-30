1f0b48c86d45660c19d32681d26de38ed091a979
Matt Miller <matt.f.miller@live.com>
2018-03-30 10:28:54 -0700

    Add loop to beepBoop in scripts.js to achieve seventh spec.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,19 @@
 function beepBoop(number) {
   var numberArray = number.toString().split("");
   console.log(numberArray);
+  // for (var i = 0; i < numberArray.length; i++) {
+
+  // }
+  for (var i = 0; i < numberArray.length; i++) {
+    if (numberArray[i] === "1") {
+      return "Boop!"
+    }
+  }
   for (var i = 0; i < numberArray.length; i++) {
     if (numberArray[i] === "0") {
       return "Beep!"
-    } else if (numberArray[i] === "1") {
-      return "Boop!"
-    } else if (number % 3 === 0) {
+    } else if (number > 0 && number % 3 === 0) {
       return "I'm sorry, Dave, I'm afraid I can't do that."
-    } else {
-      return number;
     }
   }
 }
