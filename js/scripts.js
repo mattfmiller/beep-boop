8a58d8b78b0ad9e6e5305da0f2ef3fc2bea4ead1
Matt Miller <matt.f.miller@live.com>
2018-03-30 10:14:59 -0700

    Modify branch in beepBoop to achieve fifth spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function beepBoop(number) {
   var numberArray = number.toString().split("");
   console.log(numberArray);
   for (var i = 0; i < numberArray.length; i++) {
-    if (!(numberArray[i] > 0)) {
+    if (numberArray[i] === "0") {
       return "Beep!"
-    } else if (number === 1) {
+    } else if (numberArray[i] === "1") {
       return "Boop!"
     } else if (number === 3) {
       return "I'm sorry, Dave, I'm afraid I can't do that."
