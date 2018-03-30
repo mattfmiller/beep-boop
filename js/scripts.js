9a8cbdca999a1615efeb9a9d3e10e2412462a26b
Matt Miller <matt.f.miller@live.com>
2018-03-30 09:01:48 -0700

    Add user logic to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 // Business Logic
+function beepBoop(number) {
 
+}
 
 
 
 
 //User Logic
+$(function () {
+  $("#form").submit(function(event) {
+    var userInput = $("#user-input").val();
+    var beepBoopOutput = beepBoop(userInput);
+    $("#output").text(beepBoopOutput);
+    $("#output").show();
+    event.preventDefault();
+  });
+});
