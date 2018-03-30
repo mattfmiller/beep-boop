fb18622979916794fabfcc306e212d186be7e6fc
Matt Miller <matt.f.miller@live.com>
2018-03-30 14:41:00 -0700

    Capitalize user's name in output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // Business Logic
 function beepBoop(number, name) {
+  var nameCapitalized = name.charAt(0).toUpperCase() + name.substr(1);
+  console.log(nameCapitalized);
   //Creates array from 0-number
   var numbersRange = []
   for (var i = 0; i <= number; i++) {
@@ -9,7 +11,7 @@ function beepBoop(number, name) {
   var outputArray = []
   for (var i = 0; i < numbersRange.length; i++) {
     if (numbersRange[i] > 0 && numbersRange[i] % 3 === 0) {
-      outputArray.push("I'm sorry, " + name + ", I'm afraid I can't do that." + " ");
+      outputArray.push("I'm sorry, " + nameCapitalized + ", I'm afraid I can't do that." + " ");
     } else if (numbersRange[i].toString().split("").includes("1")) {
       outputArray.push("Boop!" + " ");
     } else if (numbersRange[i].toString().split("").includes("0")) {
